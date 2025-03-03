4df513c9a354f90a0b243885d577e150f516cb62
kunal-agrawall <kunalgarg081@gmail.com>
2025-03-03 11:57:32 +0530

    Mar 03, 2025 file

--- Mar 03.py
+++ Mar 03.py
@@ -15,7 +15,30 @@ import numpy as np
 # arr = np.array([1,2,3,4,5,6])
 # arr = np.array(arr, dtype='float')
 
+# A = np.array([[1, 2], 
+#               [3, 4]])
+
+# B = np.array([[5, 6], 
+#               [7, 8]])
+
+# arr = np.matmul(A, B)
+# arr = A@B
+
+# lst = [1,2,3,4,5,6,7]
+# tup = (1,2,3,4,5,6,7)
+# arr = np.array([lst, tup])
+
+# arr = np.empty((3,3))
+
+# a = np.array([100+6j, 150+3j])
+# print(f"Real Part : {a.real}")
+# print(f"Imag Part : {a.imag}")
+
+# a = np.array([1,2,3])
+# print(f"Size of array    : {a.size}")
+# print(f"Memory of 1 item : {a.nbytes//a.size}")
+# print(f"Memory of array  : {a.nbytes}")
 
 
-print(arr)
+# print(a)
 
