7aa912be65eb17525019c69915fe6a14ac9ab3d6
kunal-agrawall <kunalgarg081@gmail.com>
2025-03-11 13:20:12 +0530

    Mar 11, 2025 file

---
+++ Mar 11.py
@@ -0,0 +1,97 @@
+# # Stack
+
+# class Stack:
+#     def __init__(self):
+#         self.stack = []
+
+#     def push(self, item):
+#         self.stack.append(item)
+    
+#     def pop(self):
+#         if not self.is_empty():
+#             return self.stack.pop()
+#         return "Stack is empty"
+    
+#     def peek(self):
+#         if not self.is_empty():
+#             return self.stack[-1]
+#         return "Stack is empty"
+    
+#     def is_empty(self):
+#         return len(self.stack) == 0
+    
+#     def size(self):
+#         return len(self.stack)
+
+
+# s = Stack()
+# s.push(10)
+# s.push(20)
+# s.push(30)
+# s.push(60)
+# s.push(90)
+# s.push(100)
+
+# print(s.pop())  
+# print(s.peek())  
+# print(s.size()) 
+
+# # /////////////////////////////////////////////////////////////////////
+
+# Using list
+
+# stack = []
+
+# stack.append(10)
+# stack.append(20)
+# stack.append(30)
+
+# print("Stack after pushing elements:", stack)
+
+# top = stack.pop()
+# print("Popped item:", top)
+# print("Stack after popping an element:", stack)
+
+# top = stack[-1]
+# print("Top element:", top)
+
+# if not stack:
+#     print("Stack is empty")
+# else:
+#     print("Stack is not empty")
+
+# //////////////////////////////////////////////////////////////
+# from collections import deque
+
+# stack = deque()
+# stack.append(1)
+# stack.append(2)
+# print(stack.pop())  
+
+# //////////////////////////////////////////////////////
+'''
+Write a function is_valid_parentheses(s: string) -> bool 
+that returns True if input string is valid, and False otherwise.
+'''
+def is_valid_parentheses(s: str) -> bool:
+    stack = []
+    
+    for char in s:
+        if char in "({[":
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            if (char == ")" and stack[-1] == "(") or \
+               (char == "]" and stack[-1] == "[") or \
+               (char == "}" and stack[-1] == "{"):
+                stack.pop()
+            else:
+                return False
+
+    return not stack
+
+
+print(is_valid_parentheses("{[]}")) 
+print(is_valid_parentheses("([)]")) 
+print(is_valid_parentheses("[{}]")) 
